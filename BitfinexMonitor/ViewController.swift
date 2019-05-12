9b2756052c1144849ea534bfa6494596ad5a3105
Paolo Musolino <info@codeido.com>
2019-05-12 15:14:08 +0200

    Initial Commit

---
+++ BitfinexMonitor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BitfinexMonitor
+//
+//  Created by Paolo Musolino on 12/05/2019.
+//  Copyright © 2019 Bitfinex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
